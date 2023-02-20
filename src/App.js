f0fb62119f2ddd022c5a3d53af0da16a2c734f0c
cortega <cortega@acciontip.cl>
2023-02-20 17:29:54 -0300

    filter

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ function App() {
   const [stateProducts, setStateProducts] = useState([]);
   const [stateSearchOptions, setStateSearchOptions] = useState({
     inputSearch: "",
+  });
+  const [stateCombobox, setStateCombobox] = useState({
+    inputCombo: "Por nombre",
     filter: 0,
   });
-  const [statePagination, setStatePagination] = useState([]);
-const [selectedPage, setSelectedPage] = useState(1);
 
+  const [statePagination, setStatePagination] = useState([]);
+  const [selectedPage, setSelectedPage] = useState(0);
 
   const changeValueInputId = (e) => {
     setStateSearchOptions({
@@ -20,15 +23,53 @@ const [selectedPage, setSelectedPage] = useState(1);
     });
   };
 
-  let arrayPagination = [];
+  const ComboBox = ({ hookStateInputCombobox }) => {
+    const arrayFilter = ["Por nombre", "Mayor precio", "Menor precio"];
+    const [stateCombobox, setStateCombobox] = useState(false);
+
+    return (
+      <div className="flexColumn">
+        <input
+          onClick={() => {
+            setStateCombobox(true);
+          }}
+          value={hookStateInputCombobox[0].inputCombo}
+        ></input>
+        <div
+          style={stateCombobox ? { display: "flex" } : { display: "none" }}
+          className="flexColumn"
+        >
+          {arrayFilter.map((listArrayFilter, index) => {
+            return (
+              <h3
+                onClick={() => {
+                  setStateCombobox(false);
+                  hookStateInputCombobox[1]({
+                    ...hookStateInputCombobox[0],
+                    filter: index,
+                  });
+                  hookStateInputCombobox[1]({
+                    filter:index,
+                    inputCombo: listArrayFilter,
+                  });
+                }}
+              >
+                {listArrayFilter}
+              </h3>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
 
   useEffect(() => {
-    setStateProducts(mock(stateSearchOptions));
-  }, [stateSearchOptions]);
+    setStateProducts(mock(stateSearchOptions, stateCombobox.filter));
+  }, [stateSearchOptions,stateCombobox.filter]);
 
   useEffect(() => {
-let countPagination = 0
-
+    let countPagination = 0;
+    let arrayPagination = [];
     if (stateProducts.length % 8 == 0) {
       countPagination = Math.trunc(stateProducts.length / 8);
     } else {
@@ -38,7 +79,7 @@ let countPagination = 0
     for (let i = 0; i < countPagination; i++) {
       arrayPagination.push(i);
     }
-    setStatePagination(arrayPagination); 
+    setStatePagination(arrayPagination);
   }, [stateProducts]);
 
   return (
@@ -57,35 +98,53 @@ let countPagination = 0
           ></input>
           <div className="flexColumn">
             <h3>Ordenar por</h3>
+            <ComboBox
+              hookStateInputCombobox={[stateCombobox, setStateCombobox]}
+            />
           </div>
         </section>
         <section className="flexRow">
-          {stateProducts.slice(selectedPage* 8, selectedPage *8 +8).map((list) => {
-            return (
-              <div className="divContainerEcommerce00 flexColumn">
-                <img src=""></img>
-                <h3>{list.name}</h3>
+          {stateProducts
+            .slice(selectedPage * 8, selectedPage * 8 + 8)
+            .map((list) => {
+              return (
+                <div className="divContainerEcommerce00 flexColumn">
+                  <img src=""></img>
+                  <h3>{list.name}</h3>
 
-                <div className="flexColumn">
-                  <h4>Informacion</h4>
-                  <h4>{list.info}</h4>
-                </div>
-                <div className="flexRow">
-                  <h4>Stock</h4> <h4>{list.stock}</h4>
-                </div>
-                <div className="flexRow">
-                  <h4>Precio</h4> <h4>{list.price}</h4>
-                </div>
+                  <div className="flexColumn">
+                    <h4>Informacion</h4>
+                    <h4>{list.info}</h4>
+                  </div>
+                  <div className="flexRow">
+                    <h4>Stock</h4> <h4>{list.stock}</h4>
+                  </div>
+                  <div className="flexRow">
+                    <h4>Precio</h4> <h4>{list.price}</h4>
+                  </div>
 
-                <button>Agregar</button>
-              </div>
-            );
-          })}
+                  <button>Agregar</button>
+                </div>
+              );
+            })}
         </section>
       </main>
       <div className="FlexRow">
         {statePagination.map((listStatePagination) => {
-          return <button style={selectedPage ===listStatePagination ?{backgroundColor:"red"}:{}} onClick={()=>{setSelectedPage(listStatePagination)}}>{listStatePagination +1}</button>;
+          return (
+            <button
+              style={
+                selectedPage === listStatePagination
+                  ? { backgroundColor: "red" }
+                  : {}
+              }
+              onClick={() => {
+                setSelectedPage(listStatePagination);
+              }}
+            >
+              {listStatePagination + 1}
+            </button>
+          );
         })}
       </div>
     </div>
--- src/mock.js
+++ src/mock.js
@@ -1,81 +1,81 @@
-const mock = (SearchOptions) => {
+const mock = (SearchOptions, filterCombobox) => {
   const products = [
     {
       name: "regla",
       info: "regla largo 30cm",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 500,
     },
     {
       name: "goma",
       info: "goma blanca de borrar",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 4200,
     },
     {
       name: "lapizes de colores",
       info: "30 lapizes de colores",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 4400,
     },
     {
       name: "tijeras",
       info: "tijeras rojas plastico",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 6400,
     },
     {
       name: "tijeras",
       info: "tijeras rojas plastico",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 8400,
     },
     {
       name: "tijeras",
       info: "tijeras rojas plastico",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 200,
     },
     {
       name: "tijeras",
       info: "tijeras rojas plastico",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 800,
     },
     {
       name: "tijeras",
       info: "tijeras rojas plastico",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 1000,
     },
     {
       name: "tijeras",
       info: "tijeras rojas plastico",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 11100,
     },
     {
       name: "regla",
       info: "regla largo 30cm",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 8400,
     },
     {
       name: "goma",
       info: "goma blanca de borrar",
       stock: 10,
       urlImg: "",
-      price: 400,
+      price: 4400,
     },
     {
       name: "lapizes de colores",
@@ -133,6 +133,45 @@ const mock = (SearchOptions) => {
       number.name.slice(0, SearchOptions.inputSearch.length) ==
       SearchOptions.inputSearch
   );
+console.log(filterCombobox,result);
+  switch (filterCombobox) {
+    case 0:
+      result.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1;
+        }
+        if (a.name < b.name) {
+          return -1;
+        }
+        return 0;
+      });
+      console.log(result);
+      break;
+    case 1:
+      result.sort((a, b) => {
+        if (a.price < b.price) {
+          return 1;
+        }
+        if (a.price > b.price) {
+          return -1;
+        }
+        return 0;
+      });
+      break;
+    case 2:
+      result.sort((a, b) => {
+        if (a.price > b.price) {
+          return 1;
+        }
+        if (a.price < b.price) {
+          return -1;
+        }
+        return 0;
+      });
+      break;
+    default:
+      break;
+  }
 
   return result;
 };
